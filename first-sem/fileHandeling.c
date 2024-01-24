8f163c74dfd3735de553065db5c3fae4f70cb4ca
Akash-nath29 <anath5440@gmail.com>
2024-01-24 11:27:03 +0530

    second commit

---
+++ first-sem/fileHandeling.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAXCONTENT 30
+
+int main(){
+	
+	FILE *ptr = NULL;
+	printf("%d", MAXCONTENT);
+//	ptr = fopen("myText.txt", "r");
+//	char c = fgetc(ptr);
+//	printf("%c", c);
+//	char str[10];
+//	fgets(str, 20, ptr);
+//	printf("%s", str);
+//	printf("%d", sizeof(str));
+
+//	ptr = fopen("myText.txt", "w");
+//	fputc('O', ptr);
+
+//	ptr = fopen("myText.txt", "r+");
+//	fputc('A', ptr);
+//	fputs("This is Akash", ptr);
+
+//	ptr = fopen("myText.txt", "w+");
+//	fputc('A', ptr);
+//	fputs("This is Akash", ptr);
+
+//	ptr = fopen("myText.txt", "a+");
+//	fputc('A', ptr);
+//	fputs("This is Akash", ptr);
+	
+	return 0;
+}
